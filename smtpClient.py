2fb3543fc78ed37f77445beb4caaf6b4ee0feb41
gfs7337 <gfs7337@nyu.edu>
2024-10-12 11:38:55 -0400

    Version 25
    v25

--- smtpClient.py
+++ smtpClient.py
@@ -65,6 +65,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     quit = 'QUIT\r\n'
     clientSocket.send(quit.encode())
+    recv7 = clientSocket.recv(1024).decode()
     # Fill in end
 
 
